7e86ca57f778c6ededf14b495a95368ebb732dc5
lucanalini <lucanalini@gmail.com>
2016-11-07 00:29:48 +0100

    lab2 Luca

--- Lab2/prim.m
+++
@@ -1,4 +0,0 @@
-function [y]=prim(x)
-for i=1:length(x)
-  y(i)=sum(x(1:i));
-end
--- Lab2/x2y.m
+++
@@ -1,6 +0,0 @@
-function [y]=x2y(x)
-y=zeros(1,length(x));
-for i=2:length(x)
-    y(i)=x(i) + 2*y(i-1);
-end
-y(1)= x(1);
